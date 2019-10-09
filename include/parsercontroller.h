46b4bec7e38fab309d48eeb0ea1f60bad2803528
Michel Megens <michel@michelmegens.net>
2019-10-09 15:10:59 +0200

    controllers: remove redundant file headers
    Remove redundant file headers from the base controller.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- include/parsercontroller.h
+++ include/parsercontroller.h
@@ -12,28 +12,10 @@
 
 namespace api
 {
-	class ParserController : public cfx::BasicController, cfx::Controller {
-		void handleGet(http_request message) override;
-
-		void handlePut(http_request message) override;
-
-		void handlePost(http_request message) override;
-
-		void handleDelete(http_request message) override;
-
-		void handlePatch(http_request messge) override;
-
-		void handleHead(http_request message) override;
-
-		void handleOptions(http_request message) override;
-
-		void handleTrace(http_request message) override;
-
-		void handleConnect(http_request message) override;
-
-		void handleMerge(http_request message) override;
-
+	class ParserController : public api::BasicController {
 	public:
+		void handleGet(http_request message) ;
+		void handlePost(http_request message) ;
 		void initRestOpHandlers() override;
 
 	};
